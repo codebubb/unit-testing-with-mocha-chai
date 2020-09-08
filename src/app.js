252bb439a66292d9d9c938c03d2a1063f084b2b2
James Bubb <jpbubb82@gmail.com>
2020-09-08 16:29:30 +0100

    lesson 01 start

--- src/app.js
+++ src/app.js
@@ -1,20 +1,5 @@
-const add = (a = 0, b = 0) => {
-    if (typeof a !== 'number' || typeof b !== 'number') {
-        return 0;
-    }
-
-    return a + b;
-};
-
-const sub = (a = 0, b = 0) => {
-    if (typeof a !== 'number' || typeof b !== 'number') {
-        return 0;
-    }
-
-    return a - b
-};
+const add = (a, b)=> a + b;
 
 module.exports = {
     add,
-    sub,
 };
